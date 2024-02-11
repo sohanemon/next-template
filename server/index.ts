de3034dc6411116ef62f9d132f1c5ca2a4d42123
sohanemon <m.sohanemon@gmail.com>
2024-02-11 13:31:59 +0600

    update trpc

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,8 @@
 import welcomeRouter from './routers/welcome';
+import { router } from './trpc';
 
-export const appRouter = welcomeRouter;
+export const appRouter = router({
+  welcome: welcomeRouter,
+});
 
 export type AppRouter = typeof appRouter;
