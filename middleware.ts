876bf6ccf09f934b7714b25b115b4deb0df3fbdc
sohanemon <m.sohanemon@gmail.com>
2024-03-27 12:13:51 +0600

    feat(middleware.ts): added URL matcher config

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,7 @@ import { withRedirect } from './lib/middlewares/with-redirect';
 const middlewares = [withRedirect];
 
 export default stackMiddlewares(middlewares);
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
