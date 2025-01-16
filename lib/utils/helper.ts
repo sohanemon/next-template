ba47c095f4ceb8ae6b175a881dab6a55e64a8789
sohanemon <sohanemon@outlook.com>
2025-01-16 13:47:28 +0600

    refactor(lib/utils/helper.ts): updated getCookieStore function to include additional methods

--- lib/utils/helper.ts
+++ lib/utils/helper.ts
@@ -1,7 +1,21 @@
 import { isSSR } from '@sohanemon/utils/core';
 
 export const getCookieStore = async () => {
-  if (isSSR) return (await import('next/headers')).cookies();
+  if (isSSR) {
+    const c = await (await import('next/headers')).cookies();
+    return {
+      get: c.get,
+      delete: c.delete,
+      has: c.has,
+      set: (name: string, value: string, day = 30) =>
+        c.set(name, value, {
+          expires: new Date().getTime() + day * 24 * 60 * 60 * 1000,
+          path: '/',
+          secure: false,
+          httpOnly: false,
+        }),
+    };
+  }
 
   const mod = await import('@sohanemon/utils/core');
   return {
