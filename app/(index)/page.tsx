6647d81d23b2343ba5ea006eff527db4e844b565
sohanemon <sohanemon@outlook.com>
2025-04-13 16:32:51 +0600

    refactor(page.tsx): updated grid and css classes

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
   return (
     <section className="container-mini min-h-[2000vh]">
       <Motion initial="bottom">
-        <h4 className="py-20 text-center container-mini text-7xl">
+        <h4 className="container-mini py-20 text-center text-7xl">
           Update Readme, Site.ts and package.json
         </h4>
       </Motion>
